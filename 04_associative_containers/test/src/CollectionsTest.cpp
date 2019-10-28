1e91a06078bacdc804c75a32e88bc6192dd88d2d
KomputerAGH <kulkejszyn@gmail.com>
2019-10-28 17:33:21 +0100

    [red] addded ElementsGreaterThanGivenValue test

--- 04_associative_containers/test/src/CollectionsTest.cpp
+++ 04_associative_containers/test/src/CollectionsTest.cpp
@@ -223,7 +223,7 @@ TEST(SetValue, CustomTypeAndComparator) {
     EXPECT_EQ(1u, set.count(Value{3, 3}));
     EXPECT_EQ(1u, set.count(Value{6, 6}));
 }
-/*
+
 TEST(SetInt, ElementsGreaterThanGivenValue) {
 
     // TODO: ....
@@ -242,7 +242,7 @@ TEST(SetInt, ElementsGreaterThanGivenValue) {
     EXPECT_EQ(5, *iter++);
     EXPECT_EQ(set.end(), iter);
 }
-
+/*
 TEST(MapStringString, CreateUsingInitializerList) {
 
     // TODO: ....
